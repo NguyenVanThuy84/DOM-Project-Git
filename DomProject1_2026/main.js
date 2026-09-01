8154267a05e9ccc01b42c1dacb1a624b53327646
NguyenVanThuy84 <106532457+NguyenVanThuy84@users.noreply.github.com>
2026-09-01 15:13:44 +0700

    Init Rep

---
+++ DomProject1_2026/main.js
@@ -0,0 +1,8 @@
+// HTML DOM
+// 1. element: id, class, tag, seclector, HTML collection
+// 2. Attribute
+// 3. Text
+console.log(document);
+document.write ('hello world');
+var headingNode = document.getElementById('heading');
+console.log (headingNode);
